ba32dcb1c798306139fac68b781359d5eab003e1
Richard <richardanjer@gmail.com>
2026-01-24 20:55:10 +0100

    Fix environment server

--- app/lib/supabase/server.tsx
+++ app/lib/supabase/server.tsx
@@ -1,9 +1,24 @@
-import { createServerClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
+import { createServerClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
 
-export const createSupabaseServerClient = () =>
-  createServerClient(
+export function createSupabaseServerClient() {
+  const cookieStore = cookies()
+
+  return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    { cookies }
-  );
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value
+        },
+        set(name: string, value: string, options: any) {
+          cookieStore.set({ name, value, ...options })
+        },
+        remove(name: string, options: any) {
+          cookieStore.set({ name, value: '', ...options })
+        },
+      },
+    }
+  )
+}
